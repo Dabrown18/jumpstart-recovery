844f2a48230759b2fdace156d817f655372f43f2
Darron Brown <DarronBrown88@outlook.com>
2018-07-28 20:49:50 -0600

    header intro

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const logoBlack = require('../../img/jumpstart-recovery-black.png');
+
+const Header = () => (
+	<div className="bg-foreground">
+		<div className="content">
+			<img src={logoBlack} alt="Jumpstart Logo" className="logo-black"/>
+			<h1 className="page-title">Bounce back from an earthquake</h1>
+			<h2 className="page-subtitle">
+				With simple quake insurance to recover from life’s disruption.
+			</h2>
+		</div>
+	</div>
+);
+
+export default Header;
