d84b44307e39af6de0f02c03ea756fcf7ce72319
Darron Brown <DarronBrown88@outlook.com>
2018-07-29 08:41:36 -0600

    added carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ImageCarousel from './ImageCarousel';
 import './Carousel.css'
 
 const Carousel = () => (
 	<div className="damage-instances">
 		<h2>Money when you need it the most</h2>
+		<ImageCarousel/>
 	</div>
 );
 
