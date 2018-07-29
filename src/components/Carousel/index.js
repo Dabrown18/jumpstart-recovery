f3340fdb578bca29cb2e831c41dc99900af64d70
Darron Brown <DarronBrown88@outlook.com>
2018-07-29 07:59:25 -0600

    carousel title

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Carousel.css'
+
+const Carousel = () => (
+	<div className="damage-instances">
+		<h2>Money when you need it the most</h2>
+	</div>
+);
+
+export default Carousel;
