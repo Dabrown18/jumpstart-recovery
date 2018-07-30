f394cba57cef318296bf407bb446f0e193dceb16
Darron Brown <DarronBrown88@outlook.com>
2018-07-30 17:46:03 -0600

    completed testimonial image one

--- src/components/BackOnTrack/index.js
+++ src/components/BackOnTrack/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './BackOnTrack.css';
 
+const IndianGuy = require('../../img/indian-guy.png');
+
 const BackOnTrack = () => (
 	<div className="back-on-track-container">
 		<h2>Back on track.</h2>
 		<p>You’re a resourceful person. Don’t get your life disrupted for too long. Speed up your recovery with Jumpstart.</p>
 		<div className="people-container">
 			<div className="foreground">
-				Hello
+				<div className="background-1">
+				</div>
+				<img
+					src={IndianGuy}
+					alt="Male Testimonial"
+					className="male-testimonial"
+				/>
 			</div>
 			<div className="foreground">
-				Hello
+				<div className="background-2">
+					<img src="" alt=""/>
+					Hello
+				</div>
 			</div>
 		</div>
 	</div>
