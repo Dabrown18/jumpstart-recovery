370fac0d816ffc7625c1bf2b82bc4715575229d3
Darron Brown <DarronBrown88@outlook.com>
2018-08-01 08:40:48 -0600

    completed footer signup form

---
+++ src/components/SignUp/SignupForm/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form'
+
+const FooterForm = () => (
+	<div className="footer-form">
+		<h2 className="form-title">
+			<p className="primary">Early Access</p>
+			<p className="secondary">Contact us to inquire. Web sales coming soon.</p>
+		</h2>
+		<Form />
+	</div>
+);
+
+export default FooterForm;
